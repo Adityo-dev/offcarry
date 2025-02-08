936f1787113bc656120a7720a5b0130d07fcc92f
Aditto dev Barmon (koushik) <134207015+Adityo-dev@users.noreply.github.com>
2025-02-09 00:22:26 +0600

    BANNER3 AND BEST SLLERS TABS ADD

---
+++ src/components/bannerGroup/Banner3.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+function Banner3() {
+  return (
+    <section className="w-full h-full">
+      <Image
+        src={"/images/banner3.png"}
+        width={1600}
+        height={1600}
+        alt=""
+        className="w-full h-full object-cover"
+      />
+    </section>
+  );
+}
+
+export default Banner3;
