228d77c9262efaf1c03dc07378aef103663ff2d4
Aditto dev Barmon (koushik) <134207015+Adityo-dev@users.noreply.github.com>
2025-02-08 12:56:19 +0600

    CATEGORY ADD

--- src/components/home/hero/Hero.js
+++ src/components/home/hero/Hero.js
@@ -5,6 +5,9 @@ function Hero() {
     <section className="w-full h-[75vh] bg-[#F1F1F1]">
       <div className="container mx-auto px-4 pt-6">
         <Category />
+        <div>
+          <p>Hi</p>
+        </div>
       </div>
     </section>
   );
