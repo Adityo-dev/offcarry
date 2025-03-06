53cc897911eeaee846010557f053ca4dd103a8dd
Aditto dev Barmon (koushik) <134207015+Adityo-dev@users.noreply.github.com>
2025-03-07 02:01:22 +0600

    Add to Cart add

---
+++ src/app/product/[id]/_component/AddToCartButton.js
@@ -0,0 +1,24 @@
+function AddToCartButton() {
+  return (
+    <div>
+      <button class="group w-40 h-12 rounded-lg border-none bg-secondary flex items-center justify-center cursor-pointer transition-all duration-500 overflow-hidden relative">
+        <span class="absolute left-[-50px] w-[30px] h-[30px] bg-transparent rounded-full flex items-center justify-center overflow-hidden z-[2] transition-all duration-500 group-hover:translate-x-[58px] group-hover:rounded-[40px]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            viewBox="0 0 576 512"
+            fill="rgb(17, 17, 17)"
+            class="cart"
+          >
+            <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path>
+          </svg>
+        </span>
+        <p class="h-full w-fit flex items-center justify-center text-white z-[1] transition-all duration-500 text-[1.04em] font-semibold group-hover:translate-x-[10px]">
+          Add to Cart
+        </p>
+      </button>
+    </div>
+  );
+}
+
+export default AddToCartButton;
