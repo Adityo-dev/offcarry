bbc1840cf776fb229af136ac4710ebed9e1f7f17
Aditto dev Barmon (koushik) <134207015+Adityo-dev@users.noreply.github.com>
2025-04-09 17:25:39 +0600

    all Quick Links add and social Media add

--- src/components/home/brand/Brand.js
+++ src/components/home/brand/Brand.js
@@ -1,65 +1,69 @@
 "use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
-
-// import marquee
 import Marquee from "react-fast-marquee";
+import Link from "next/link";
 
-const brandData = [
-  "/logo/sony-1.png",
-  "/logo/dell.png",
-  "/logo/lenovo.png",
-  "/logo/samsung.png",
-  "/logo/sanyo-1.png",
-  "/logo/oppo.png",
-  "/logo/panasonic.png",
-  "/logo/asus.png",
-  "/logo/lenovo.png",
-  "/logo/samsung.png",
-  "/logo/sanyo-1.png",
-  "/logo/oppo.png",
-  "/logo/panasonic.png",
-  "/logo/asus.png",
-];
+function Brand() {
+  const [brandData, setBrandData] = useState([]);
 
-const Brand = () => {
+  useEffect(() => {
+    async function fetchBrands() {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_ROUT_URL}/shop/brands`,
+          { cache: "no-cache" }
+        );
+        const data = await response.json();
+        setBrandData(data);
+      } catch (error) {
+        console.error("Failed to fetch brand data:", error);
+      }
+    }
+    fetchBrands();
+  }, []);
+
   return (
     <div className="relative w-full space-y-4 lg:space-y-6 overflow-hidden">
-      {/*  BRAND MARQUEE LEFT */}
-      <Marquee className="w-full mx-auto" pauseOnHover="true">
+      {/* BRAND MARQUEE LEFT */}
+      <Marquee className="w-full mx-auto" pauseOnHover={true}>
         {brandData.map((data, ind) => (
-          <div
-            className="w-40 lg:w-52 xl:w-64 h-20 lg:h-28 xl:h-36 border flex items-center justify-center rounded-lg ml-4 lg:ml-6 cursor-pointer"
+          <Link
+            href={`/brand/${data?.name?.toLowerCase()}`}
             key={ind}
+            className="w-40 lg:w-52 xl:w-64 h-20 lg:h-28 xl:h-36 border flex items-center justify-center rounded-lg ml-4 lg:ml-6 cursor-pointer"
           >
             <Image
-              src={data}
+              src={data?.logo}
               width={400}
               height={400}
               alt={`Logo of ${data?.name || "Brand"}`}
-              className="w-20 lg:w-28 xl:w-36"
+              className="w-20 lg:w-28 xl:w-32 h-auto "
             />
-          </div>
+          </Link>
         ))}
       </Marquee>
-      {/*  BRAND MARQUEE RIGHT */}
-      <Marquee className="w-full mx-auto" pauseOnHover="true" direction="right">
+
+      {/* BRAND MARQUEE RIGHT */}
+      <Marquee className="w-full mx-auto" pauseOnHover={true} direction="right">
         {brandData.map((data, ind) => (
-          <div
-            className="w-40 lg:w-52 xl:w-64 h-20 lg:h-28 xl:h-36  border flex items-center justify-center rounded-lg ml-4 lg:ml-6 cursor-pointer"
+          <Link
+            href={`/brand/${data?.name?.toLowerCase()}`}
             key={ind}
+            className="w-40 lg:w-52 xl:w-64 h-20 lg:h-28 xl:h-36 border flex items-center justify-center rounded-lg ml-4 lg:ml-6 cursor-pointer"
           >
             <Image
-              src={data}
+              src={data?.logo}
               width={400}
               height={400}
               alt={`Logo of ${data?.name || "Brand"}`}
-              className="w-20 lg:w-28 xl:w-36"
+              className="w-20 lg:w-28 xl:w-32 h-auto "
             />
-          </div>
+          </Link>
         ))}
       </Marquee>
     </div>
   );
-};
+}
 
 export default Brand;
